81b8edb633ec737b720a1f606e775f9e22319c28
Salimcan Satıcı <salimcan@salimcan.com>
2020-04-29 04:36:31 +0300

    Çalışır halde güncellendi açıklamayı okuyunuz
    Önceki versiyona kıyasla,
    - Bilgisayar tarihine göre çizim yapılma kodları kaldırıldı artık bu sayacı sizler manuel belirleyeceksiniz.
    - 5 saniyede bir fotoğraf değiştirilmesi sağlandı.
    - Kodlar kısaltıldı
    - SMS ya da engel yememek adına ilk önce instagram.com'a yazacağınız hesap ile manuel giriş yapmanız gerekmekte.

--- app.js
+++ app.js
@@ -1,76 +1,12 @@
-const puppeteer = require('puppeteer'); //Salimcan Satıcı 
+const puppeteer = require('puppeteer'); //Salimcan Satıcı
 const fs = require('fs');
-var Jimp = require('jimp'); //
+var Jimp = require('jimp'); 
 
 const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36';
 (async () => {
 
 //Yurtdışında birinde görmüş olduğum programı Nodejs + Puppeteer chrome kullanarak yazdım. 
 //Piyasada ilklerdendir
-
-var sayilar = ["./dakikalar/0.png",
-"./dakikalar/1.png",
-  "./dakikalar/2.png",
-  "./dakikalar/3.png",
-  "./dakikalar/4.png",
-  "./dakikalar/5.png",
-  "./dakikalar/6.png",
-  "./dakikalar/7.png",
-  "./dakikalar/8.png",
-  "./dakikalar/9.png",
-  "./dakikalar/10.png",
-  "./dakikalar/11.png",
-  "./dakikalar/12.png",
-  "./dakikalar/13.png",
-  "./dakikalar/14.png",
-  "./dakikalar/15.png",
-  "./dakikalar/16.png",
-  "./dakikalar/17.png",
-  "./dakikalar/18.png",
-  "./dakikalar/19.png",
-  "./dakikalar/20.png",
-  "./dakikalar/21.png",
-   "./dakikalar/22.png",
-    "./dakikalar/23.png",
-     "./dakikalar/24.png",
-     "./dakikalar/25.png",
-     "./dakikalar/26.png",
-     "./dakikalar/27.png",
-     "./dakikalar/28.png",
-     "./dakikalar/29.png",
-     "./dakikalar/30.png",
-     "./dakikalar/31.png",
-     "./dakikalar/32.png",
-     "./dakikalar/33.png",
-     "./dakikalar/34.png",
-     "./dakikalar/35.png",
-     "./dakikalar/36.png",
-     "./dakikalar/37.png",
-     "./dakikalar/38.png",
-     "./dakikalar/39.png",
-     "./dakikalar/40.png",
-     "./dakikalar/41.png",
-     "./dakikalar/42.png",
-     "./dakikalar/43.png",
-     "./dakikalar/44.png",
-     "./dakikalar/45.png",
-     "./dakikalar/46.png",
-     "./dakikalar/47.png",
-     "./dakikalar/48.png",
-     "./dakikalar/49.png",
-     "./dakikalar/49.png",
-     "./dakikalar/50.png",
-     "./dakikalar/51.png",
-     "./dakikalar/52.png",
-     "./dakikalar/53.png",
-     "./dakikalar/54.png",
-     "./dakikalar/55.png",
-     "./dakikalar/56.png",
-     "./dakikalar/57.png",
-     "./dakikalar/58.png",
-     "./dakikalar/59.png",
-     "./dakikalar/00.png",];
-
 
     const browser = await puppeteer.launch({
       headless: false,
@@ -83,20 +19,19 @@ var sayilar = ["./dakikalar/0.png",
     const page = await browser.newPage();
     console.log("Browser açılıyor");
   await page.setUserAgent(USER_AGENT);
-  console.log("Useragent tanımlandı");
+  // console.log("Useragent tanımlandı");
   await page.goto('https://www.instagram.com/accounts/edit/');
-  console.log("Instagram edit yönlendirme sağlandı");
+  // console.log("Instagram edit yönlendirme sağlandı");
   await page.waitForSelector('input[name="username"]'); 
-  console.log("Girilecek text alanının yüklenmesini bekliyorum");
+   //console.log("Girilecek text alanının yüklenmesini bekliyorum");
 
-
-  await page.type('input[name="username"]', 'KULLANICIADINIZIIBURAYAGIRIN'); //'KULLANICI_ADINIZ '  KULLANICI_ADINIZ yazan kısma yani tırnakları silmeden tırnakların içerisine kullanıcı adınızı yazınız
-  console.log("Kullanıcı adı textbox'ına kullanıcı adınız yazılıyor");
-  await page.type('input[name="password"]', 'SIFRENIZIBURAYAGIRIN'); // Aynı şekilde SIFRENIZ kısmına da o şekilde boşluk bırakmayın
-  console.log("Şifreniz textbox'ına şifreniz yazılıyor. Programda kesinlikle bir veri gönderimi gibi bir durum yok aksi durumda kodları inceleyin inceletiniz. Sorumluluk size aittir.");
+  await page.type('input[name="username"]', 'burayakullanıcıadınız'); //'KULLANICI_ADINIZ '  KULLANICI_ADINIZ yazan kısma yani tırnakları silmeden tırnakların içerisine kullanıcı adınızı yazınız
+  // console.log("Kullanıcı adı textbox'ına kullanıcı adınız yazılıyor");
+  await page.type('input[name="password"]', 'şifreniz'); // Aynı şekilde SIFRENIZ kısmına da o şekilde boşluk bırakmayın
+ // console.log("Şifreniz textbox'ına şifreniz yazılıyor. Programda kesinlikle bir veri gönderimi gibi bir durum yok aksi durumda kodları inceleyin inceletiniz. Sorumluluk size aittir.");
 
   await page.click('button[type="submit"]');
-  console.log("Giriş yap butonuna tıkladım.")
+  // console.log("Giriş yap butonuna tıkladım.")
   blockingWait(4);
   await page.close(); //Navigation hatasını handle etmek biraz sıkıntılı ve kullandığımız modülde hata vermemesi için bir nebze page variable transfer yapıldı
   const sekme2 = await browser.newPage();
@@ -104,70 +39,27 @@ var sayilar = ["./dakikalar/0.png",
   await sekme2.goto('https://www.instagram.com/accounts/edit/');
   const pageTitle = await sekme2.title();
   console.log(pageTitle)
+  var inputElement = await sekme2.$('#react-root > section > main > div > article > div > div.LqNQc > div > div > form > input[type="file"]'); 
+ 
+  var photoNum=1; // 1.jpg adlı fotoğraftan başlatılır
 
+var fotogradedi=24; //Fotoğraf adediniz kaç taneyse o kadar yazınız.
 
-  // for (let index = 0; index < 9999; index++) { //index = 0; kısmı kaçıncı dakikadan başlayacağını belirler v1
-    
-blockingWait(2); 
-
-const inputElement = await sekme2.$('#react-root > section > main > div > article > div > div.XX1Wc > div > form > input[type="file"]'); 
-
-    setInterval(function(){ 
-
-      let date = new Date();
-      let simdikiDakika = date.getMinutes();
-      let simdikiSaat = date.getHours();
-      simdikisaat = simdikiSaat+1;
-      //Tarih'i çektik ***************************
-let kacinciGun = date.getDate();
-let kacinciayTemp= date.getMonth();
-let kacinciAy = kacinciayTemp+1;
-let kacinciYil = date.getFullYear();
-//******************************* */
-
-var filePath = './dakikalar/'+simdikiDakika+'.png'; 
-  fs.unlinkSync(filePath); 
-  console.log(filePath+" SİLİNDİ");
-
- //http://lorempixel.com/320/320/ random fotoğraf çekmek için eklendi dilerseniz beyaz arkaplan bir resmin yolunu verip beyaz arkaplana da yazdırabilirsiniz.
-Jimp.read("http://lorempixel.com/320/320/").then(function (delimg) { 
-  
-Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(function (font) { 
-  delimg.blur( 20 );
-  delimg.resize(320, 320) 
-delimg.HORIZONTAL_ALIGN_CENTER; 
-                //80 Sağa / //20 Yukarı
-                // 15 sağa / // 80 yukarı
-  delimg.print(font, 80, 20, simdikiSaat+":"+simdikiDakika,80) 
-  delimg.print(font, 15, 80, kacinciGun+"/"+kacinciAy+"/"+kacinciYil,40) 
-  delimg.write('./dakikalar/'+simdikiDakika+'.png');
-
-  console.log(simdikiDakika+'.png'+" Yazıldı");
-
-
-  inputElement.uploadFile('./dakikalar/'+simdikiDakika+'.png');
-  console.log(simdikiDakika+'.png upload edildi.');
-
-
-
-
-});
-
-});    
-
-
-
-console.log("İşlem Başarılı");
-},60000); //60 SANİYEDE BİR TEKRAR
-    
-    console.log("Bağlandı");
-     
-  
+for (let tekrar = 0; tekrar < 999999999; tekrar++) {
+  for (let index = 0; index < fotogradedi; index++) {
+    blockingWait(5);
+  inputElement.uploadFile('./dakikalar/'+photoNum+'.jpg');
+ 
+  console.log("Şu numaralı bitmoji yüklenecektir -> "+photoNum);
+  photoNum++;
+  if(photoNum==fotogradedi){
+    console.log("Tekrarlanıyor");
+    photoNum=1;
   }
-
+  }
+}
+  }
 )();
-
-
 function blockingWait(seconds) {
   //basit bloklama beklemesi
   var waitTill = new Date(new Date().getTime() + seconds * 1000);
